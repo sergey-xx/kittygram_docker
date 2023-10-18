eaf879d760655469f940607a59208518b592a5e5
Sergey Mazilin <mazilin.sergey@yandex.ru>
2023-10-18 18:37:09 +0400

    привел к требованиям ТЗ 2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
-        'PORT': 5432
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
